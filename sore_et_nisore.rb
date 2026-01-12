2c1ddf44815b089efcebba2970e1d9d824b1ac28
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2026-01-12 15:06:16 -0500

    Create sore_et_nisore.rb

---
+++ sore_et_nisore.rb
@@ -0,0 +1,6 @@
+puts "Standard"
+#sore_wa(sore("pinatsu", "kuro"), shikashi("pinatsu", "chairo"), matawa("pinatsu", "kuro", "chairo", "aka"))
+sore_wa("He seterone na une nuse bode", "Hu powe rouge tu bode", "Hu powe na une ne bode")
+
+puts "Hanten-Ronri"
+nisore_wa("He seterone na une nuse bode", "Hu powe rouge tu bode", "Hu powe na une ne bode")
